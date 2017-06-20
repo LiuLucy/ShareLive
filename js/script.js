54709b44188fe15c020be75b1d088f4f8164f0e3
LiuLucy <lucy933626@gmail.com>
2017-06-21 01:09:19 +0800

    sign button hover

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(function()
+{
+    $('#sign_in_icon').hover(function()
+    {
+        $('#sign_in_icon').attr('src' , 'images/page1/web/sigin_in_hover.png');
+    },
+    function()
+    {
+        $('#sign_in_icon').attr('src', 'images/page1/web/sign_in.png');
+    });
+});
